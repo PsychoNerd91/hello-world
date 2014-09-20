19f12a8a093cc0f15457d1551fe7acc4aefa7afa
Drew Lowe <drew.lowe91@gmail.com>
2014-09-20 19:01:02 +1000

    Create HelloWorld.java
    First test file to commit.
    Am I doing this right?

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    //It's obvious what this is doing.
+    //Prints 'Hello World!' to console.
+    System.out.print("Hello World!");
+  }
+}
